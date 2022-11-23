af366f512ca2b63b02e83a9668adcf5bab2a970f
Aman yadav <aman.yadav@swiggy.in>
2022-11-24 00:51:54 +0530

    multiple ping pong v2

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 
@@ -10,12 +11,7 @@ import (
 	// "os"
 )
 
-func sayPong(conn net.Conn){
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-	conn.Write([]byte("+PONG\r\n"))
-	defer conn.Close()
-}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -27,15 +23,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	conn, err := l.Accept()
+	defer conn.Close()
 	for {
-		conn, err := l.Accept()
+
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go sayPong(conn)
+		buffer := make([]byte, 1024)
+		if _,err:=conn.Read(buffer);err!=nil{
+			if err==io.EOF{
+				break
+			}else{
+				os.Exit(1)
+			}
+		}
 
+		conn.Write([]byte("+PONG\r\n"))
 	}
-
-
+    defer l.Close()
 }

c724c265dcd14ebf640e74ee9f42d8c02912ec74
Aman yadav <aman.yadav@swiggy.in>
2022-11-24 00:01:19 +0530

    multiple ping pong

--- app/server.go
+++ app/server.go
@@ -21,14 +21,18 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		buffer := make([]byte, 1024)
+		conn.Read(buffer)
+		conn.Write([]byte("+PONG\r\n"))
+		conn.Close()
+
 	}
-	buffer := make([]byte, 1024)
-	conn.Read(buffer)
-	conn.Write([]byte("+PONG\r\n"))
-	conn.Close()
+
 
 }

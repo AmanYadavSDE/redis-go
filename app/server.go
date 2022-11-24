0d4fb16c10d49094dffff2063f073799db58d130
Aman yadav <aman.yadav@swiggy.in>
2022-11-25 01:08:49 +0530

    concurrent

--- app/server.go
+++ app/server.go
@@ -11,7 +11,21 @@ import (
 	// "os"
 )
 
+func handleClient(conn net.Conn){
+	buffer := make([]byte, 1024)
+	for {
+		if _, err := conn.Read(buffer); err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				os.Exit(1)
+			}
+		}
 
+		conn.Write([]byte("+PONG\r\n"))
+	}
+	defer conn.Close()
+}
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -23,24 +37,16 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	defer conn.Close()
-	for {
 
+
+	for {
+		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		buffer := make([]byte, 1024)
-		if _,err:=conn.Read(buffer);err!=nil{
-			if err==io.EOF{
-				break
-			}else{
-				os.Exit(1)
-			}
-		}
+		go handleClient(conn)
 
-		conn.Write([]byte("+PONG\r\n"))
 	}
     defer l.Close()
 }
